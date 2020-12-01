b92d55730d6a397dadf139ad61b1a5adbba8404c
Damian Filipek <damianfilipek81@gmail.com>
2020-12-02 00:29:16 +0100

    Add win/lose/tie/unkown situation functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//random, computer move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -17,7 +18,7 @@ else if (randomNumber == 3){
 printMessage('Ruch komputera: ' + computerMove);
 
 
-
+//player input
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -35,3 +36,38 @@ else if (playerInput == '3') {
 }
 
 printMessage('Mój ruch to: ' + playerMove);
+
+//win situation
+if (computerMove == 'kamień' && playerMove == 'papier') {
+	printMessage('Wygrywasz!');
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce') {
+	printMessage('Wygrywasz');
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+	printMessage('Wygrywasz');
+}
+//lose situation
+else if (computerMove == 'papier' && playerMove == 'papier') {
+	printMessage('Remis');
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień') {
+	printMessage('Remis');
+}
+else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+	printMessage('Remis');
+}
+//tie situation
+else if (computerMove == 'papier' && playerMove == 'kamień') {
+	printMessage('Przegrywasz');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+	printMessage('Przegrywasz');
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier') {
+	printMessage('Przegrywasz');
+}
+//uknown situation
+else {
+	printMessage('Nie wykonałeś poprawnego ruchu');
+}
